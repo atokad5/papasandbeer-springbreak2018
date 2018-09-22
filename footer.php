37cf77976f36bac5e822b394c25aa685c7973c37
atokad5 <kenny.krosky@gmail.com>
2018-09-22 11:14:17 -0700

    cache bust

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 
 
 
-<script src="<?php echo "$root/dist/assets/js/app.bundle.js?=" ?>"></script>
+<script src="<?php echo "$root/dist/assets/js/app.bundle.js?fwa=fwa" ?>"></script>
 <?php  wp_footer();  ?>
 </body>
 </html>
