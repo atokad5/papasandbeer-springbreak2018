5cc1740c4725ce38fc7130f20998254458ec848f
atokad5 <kenny.krosky@gmail.com>
2018-09-25 20:45:38 -0700

    change to load

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="<?php echo "$root/dist/assets/js/app.bundle.js?h=ga" ?>"></script>
+<script src="<?php echo "$root/dist/assets/js/app.bundle.js?gh=ga" ?>"></script>
 <?php  wp_footer();  ?>
 </body>
 </html>
