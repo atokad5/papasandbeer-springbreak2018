098545d1a9a6999e991e5a8a9b006ca4f0de9922
atokad5 <kenny.krosky@gmail.com>
2018-09-30 14:24:06 -0700

    working on footer

---
+++ includes/footer-markup.php
@@ -0,0 +1,89 @@
+<?php 
+	$root = get_template_directory_uri();
+?>
+
+<footer>
+	<div class="spacing spacing--md"></div>
+		<div class="inner footer-grid">
+
+
+				<div class="footer-column is-large">
+					<h3 class="footer-section-title">Get In Touch</h3>
+					<div class="mini-spliter animated-spliter"></div>
+					<p>CORONADO 400, PLAYAS ROSARITO</p>
+					<p>ROSARITO, BC, MEXICO<BR>(619)799-9491</p>
+					<p><a href="">CONTACT US</a></p>
+					<?php get_template_part('includes/social-links') ?>
+					<p>&copy; 2019 PAPAS AND BEER SPRING BREAK FEST ALL RIGHTS RESERVED</p>
+					<p><a href="">TERMS & CONDITIONS</a> <span> | </span> <a href="">PRIVACY POLICY</a></p>
+				</div>
+
+
+				<div class="footer-column is-small">
+
+					<div class="footer-row">
+						<h3 class="footer-section-title">EXPERIENCE</h3>
+						<div class="mini-spliter animated-spliter"></div>
+						<ul>
+							<li><a href="">ABOUT THE VENUE</a></li>
+							<li><a href="">ABOUT ROSARITO</a></li>
+						</ul>
+					</div>
+
+					<div class="footer-row">
+						<h3 class="footer-section-title">TRAVEL</h3>
+						<div class="mini-spliter animated-spliter"></div>
+						<ul>
+							<li><a href="">HOW TO GET HERE</a></li>
+							<li><a href="">CROSSING THE BORDER</a></li>
+							<li><a href="">HOTELS</a></li>
+						</ul>
+					</div>
+
+				</div>
+
+
+
+
+				<div class="footer-column is-small">
+
+					<div class="footer-row">
+						<h3 class="footer-section-title">PACKAGES</h3>
+						<div class="mini-spliter animated-spliter"></div>
+						<ul>
+							<li><a href="">WHAT'S INCLUDED</a></li>
+							<li><a href="">PRICING</a></li>
+						</ul>
+					</div>
+
+					<div class="footer-row">
+						<h3 class="footer-section-title">INFO</h3>
+						<div class="mini-spliter animated-spliter"></div>
+						<ul>
+							<li><a href="">FAQS</a></li>
+							<li><a href="">CONTACT</a></li>
+						</ul>
+					</div>
+
+				</div>
+
+
+				<div class="footer-column is-md">
+
+					<div class="footer-row">
+						<h3 class="footer-section-title">LINEUP</h3>
+						<div class="mini-spliter animated-spliter"></div>
+						<ul>
+							<li><a href="">ARTIST INFORMATION</a></li>
+						</ul>
+					</div>
+					
+				</div>
+
+
+		</div>
+
+
+
+	<div class="spacing spacing--md"></div>
+</footer>
