6baf186833fd0263426b6090e2c14aeb1fb7cee5
atokad5 <kenny.krosky@gmail.com>
2018-10-01 15:48:57 -0700

    added custom fields for image layout

--- includes/home/image-layout.php
+++ includes/home/image-layout.php
@@ -2,7 +2,7 @@
 
 <div class="is-image-column left-image-column">
 	<div class="image-nest figure-left-img">
-		<img class="lazy-image" data-lazy="<?php echo "$root/_assets/images/left-image.jpg"; ?>" src="" alt="">
+		<img class="lazy-image" data-lazy="<?php the_field('left_image_tall'); ?>" src="" alt="">
 		<div class="is-reveal-sleeve-img-group"></div>
 	</div>
 </div>
@@ -10,13 +10,13 @@
 <div class="is-image-column right-image-column">
 
 	<div class="image-nest figure-right-top-img">
-		<img class="lazy-image" data-lazy="<?php echo "$root/_assets/images/top-right-image.jpg"; ?>" src="" alt="">
+		<img class="lazy-image" data-lazy="<?php the_field('top_right_image'); ?>" src="" alt="">
 		<div class="is-reveal-sleeve-img-group"></div>
 	</div>
 
 	<div class="is-half-inner">
 		<div class="image-nest figure-right-bottom-img">
-			<img class="lazy-image" data-lazy="<?php echo "$root/_assets/images/bottom-right-image.jpg"; ?>" src="" alt="">
+			<img class="lazy-image" data-lazy="<?php the_field('bottom_right_image'); ?>" src="" alt="">
 			<div class="is-reveal-sleeve-img-group"></div>
 		</div>
 	</div>
--- templates/home.php
+++ templates/home.php
@@ -33,11 +33,11 @@
 			<div class="half-el inner clearfix is-half-inner-column">
 				<div class="is-image-grid-content">
 					<div class="content-area">
-						<h2 class="dates is-center sm-hdr">THIS IS ONLY THE BEGINNING</h2>
+						<h2 class="dates is-center sm-hdr"><?php the_field('image_grid_sub_header'); ?></h2>
 					</div>
 					<div class="section-header is-center image-layout-hdr">
 						<div class="content-area">
-							<h1>The Ultimate Party</h1>
+							<h1><?php the_field('image_grid_header'); ?></h1>
 						</div>
 					</div>
 
@@ -47,9 +47,105 @@
 						<div class="content-area">
 							<p>Have a cold drink in hand while sitting under the sun, poolside listening to amazing music in good company. When the sun sets, our beachfront club lights up and comes to life.</p>
 						</div>
+						<?php 
+							$hasParagraphTwo = get_field('image_paragraph_two');
+							if($hasParagraphTwo) :
+						?>
+							<div class="content-area">
+								<?php echo $hasParagraphTwo ?>
+							</div>
+						<?php endif; ?>
+					</div>
+					<div class="section-cta">
+						<a class="cta" href="">View Packages</a>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="half-el image-layout-parent">
+				<?php get_template_part('includes/home/image-layout'); ?>
+			</div>
+			
+			
+		
+		</div>
+		<div class="spacing spacing--md"></div>
+		<div class="spacing spacing--md"></div>
+		<div class="spacing spacing--sm"></div>
+</section>
+
+
+<?php get_template_part('includes/home/whos-next'); ?>
+
+
+<?php get_template_part('includes/home/slider'); ;?>
+<?php get_template_part('includes/home/tickets-selling') ?>
+<?php get_template_part('includes/home/faq'); ?>
+
+
+
+
+<?php get_footer(); ?>
+
+
+
+<?php /* Template Name: Home Page */ 
+	get_header(); 
+	$root = get_template_directory_uri();
+?>
+
+<?php get_template_part('includes/nav') ?>
+
+<?php get_template_part('includes/loadin'); ?>
+
+<header>
+	<div class="inner hdr-inner over-bg">
+		<div class="hdr-container pre-built">
+			<h2 class="dates hdr-el"><?php the_field('dates'); ?></h2>
+			<div class="section-header hdr-el">
+				<h1><?php the_field('hero_headline') ?></h1>
+			</div>
+			<p class="hdr-el"><?php the_field('hero_copy'); ?></p>
+			<a class="cta hdr-el hdr-cta" href="">View Packages</a>
+		</div>		
+	</div>
+	<div class="is-transition-bg"></div>
+	<div class="is-bg is-el-bg header-bg" data-bg-src="<?php the_field('hero_background') ;?>"></div>
+</header>
+
+
+
+<?php get_template_part('includes/home/video_section'); ?>
+
+
+<section class="is-orange">
+		
+		<div class="max clearfix flex-row-images">
+			<div class="half-el inner clearfix is-half-inner-column">
+				<div class="is-image-grid-content">
+					<div class="content-area">
+						<h2 class="dates is-center sm-hdr"><?php the_field('image_grid_sub_header'); ?></h2>
+					</div>
+					<div class="section-header is-center image-layout-hdr">
 						<div class="content-area">
-							<p>Papas&Beer celebrates Spring Break the entire month of March, we are ready for you to dance all night and make memories to remember. During the weekdays, JusCollege will be hosting pool parties and bringing in fire performers. On the weekends, we got you covered with more wild pool parties and several A-List performers.</p>
+							<h1><?php the_field('image_grid_header'); ?></h1>
 						</div>
+					</div>
+
+					<div class="hr-splitter animated-spliter"></div>
+
+					<div class="image-grid-copy">
+						<div class="content-area">
+							<p><?php the_field('image_paragraph_one'); ?></p>
+						</div>
+						<?php 
+							$hasParagraphTwo = get_field('image_paragraph_two');
+							if(strlen($hasParagraphTwo) > 2) : ?>
+							<div class="content-area">
+								<?php echo $hasParagraphTwo; ?>
+							</div>
+						<?php endif; ?>
 					</div>
 					<div class="section-cta">
 						<a class="cta" href="">View Packages</a>
